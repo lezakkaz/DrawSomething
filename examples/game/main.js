dbf1322f81fb0798377034f1b5a939517a4bee78
Boeing737ng <ghlove0317@naver.com>
2018-08-14 13:27:03 +0900

    Page moves on user's action

--- examples/game/main.js
+++ examples/game/main.js
@@ -1,11 +1,24 @@
-$(".comming-soon").hover(function() {
-        $(this).children('.text-wrapper').css('opacity','0.2');
-        $(this).append(
-            "<div class='comming-soon-message'>Comming Soon...</div>"
-        );
-        $(".comming-soon-message").fadeIn(200);
-    }, function() {
-        $(this).children('.text-wrapper').css('opacity','1');
-        $(".comming-soon-message").remove();
-    }
-);
+$(document).ready(function() { 
+    $(".comming-soon").hover(
+        function() {
+            $(this).children('.text-wrapper').css('opacity','0.2');
+            $(this).append(
+                "<div class='comming-soon-message'>Comming Soon...</div>"
+            );
+            $(".comming-soon-message").fadeIn(200);
+        }, function() {
+            $(this).children('.text-wrapper').css('opacity','1');
+            $(".comming-soon-message").remove();
+        }
+    );
+    $(".select-game").click(
+        function() {
+            var index = $(".select-game").index(this);
+            if(index == 0) {
+                window.location.href = 'speed_draw.html';
+            } else if(index == 1) {
+                window.location.href = 'guess_draw.html';
+            }
+        }
+    );
+});
