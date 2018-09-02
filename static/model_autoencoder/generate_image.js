6e8aa1b83d8082663fed98947aabbeeaf6a5e760
Boeing737ng <ghlove0317@naver.com>
2018-09-02 22:11:48 +0900

    Setup function runs only when raw data is set

--- static/model_autoencoder/generate_image.js
+++ static/model_autoencoder/generate_image.js
@@ -3,8 +3,6 @@ var rnn_model_data;
 var temperature = 0.1;
 var use_large_models = true;
 
-
-
 var draw_example = function(example, start_x, start_y, line_color) {
   var i;
   var x=start_x, y=start_y;
@@ -38,37 +36,38 @@ var draw_example = function(example, start_x, start_y, line_color) {
 };
 
 var setup = function() {
-  var drawing, i, x_position, cnv;
+  if(typeof model_raw_data != 'undefined') {
+    var drawing, i, x_position, cnv;
+    // make sure we enforce some minimum size of our demo
+    var screen_width = 480;
+    var screen_height = 320;
+    x_pos = 900;
+    y_pos = 900;
 
-  // make sure we enforce some minimum size of our demo
-  var screen_width = 480;
-  var screen_height = 320;
-  x_pos = 900;
-  y_pos = 900;
+    createCanvas(screen_width, screen_height);
 
-  createCanvas(screen_width, screen_height);
+    function centerCanvas() {
+      var x = (windowWidth - width) / 2;
+      var y = (windowHeight - height) / 2;
+      //cnv.position(x, y);
+    }
+    centerCanvas();
+    // declare sketch_rnn model
+    rnn_model_data = JSON.parse(model_raw_data);
+    rnn_model = new SketchRNN(rnn_model_data);
 
-  function centerCanvas() {
-    var x = (windowWidth - width) / 2;
-    var y = (windowHeight - height) / 2;
-    //cnv.position(x, y);
+    function generate(z, y_start, c) {
+      console.log(temperature);
+      x_position = 220;
+      drawing = rnn_model.decode(z, temperature);
+      drawing = rnn_model.scale_drawing(drawing, 90);
+      drawing = rnn_model.center_drawing(drawing);
+      draw_example(drawing, x_position, y_start, c);
+    }
+
+    // create a random drawing.
+    var z_0 = rnn_model.random_latent_vector();
+    generate(z_0, 140, color(220, 0, 0));
+    stopLoading();
   }
-  centerCanvas();
-  // declare sketch_rnn model
-  rnn_model_data = JSON.parse(model_raw_data);
-  rnn_model = new SketchRNN(rnn_model_data);
-
-  function generate(z, y_start, c) {
-    console.log(temperature);
-    x_position = 220;
-    drawing = rnn_model.decode(z, temperature);
-    drawing = rnn_model.scale_drawing(drawing, 90);
-    drawing = rnn_model.center_drawing(drawing);
-    draw_example(drawing, x_position, y_start, c);
-  }
-
-  // create a random drawing.
-  var z_0 = rnn_model.random_latent_vector();
-  generate(z_0, 140, color(220, 0, 0));
-  stopLoading();
 };
