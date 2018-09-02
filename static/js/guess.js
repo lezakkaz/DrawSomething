7580339a98b5f697c93287a220a479ae8716d7b3
Boeing737ng <ghlove0317@naver.com>
2018-09-02 16:00:17 +0900

    Temperature changes when go to next stage

--- static/js/guess.js
+++ static/js/guess.js
@@ -1,7 +1,29 @@
+var set_temperature = 1;
+var current_stage = 1; // 0~9
+
 $(document).ready(function() {
     $('#user-input').keypress(function(event) {
         if(event.keyCode === 13) {
             console.log($('#user-input').val());
+            moveToNextLevel();
         }
     })
 })
+
+function moveToNextLevel() {
+    if(current_stage === 10) {
+        alert("게임이 종료되었습니다.");
+        return;
+    }
+    else {
+        setStageStyle();
+        temperature -= 0.1;
+        current_stage += 1;
+        setup();
+    }
+}
+
+function setStageStyle() {
+    var stages = document.getElementsByClassName("drawing-level");
+    stages[9 - current_stage].style.opacity = "1";
+}
